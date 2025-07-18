2a1fd15fcca85f83be2c1bf005e1bf77a03c02da
= <=>
2025-07-18 19:42:07 -0300

    Nats config added

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -11,10 +11,9 @@ import { envs, PRODUCT_SERVICE } from 'src/config';
     ClientsModule.register([
       { 
         name: PRODUCT_SERVICE,
-        transport: Transport.TCP,
+        transport: Transport.NATS,
         options: {
-          host: envs.productsMicroserviceHost,
-          port: envs.productsMicroservicePort
+          servers: envs.natServers
         }
       },
     ]),

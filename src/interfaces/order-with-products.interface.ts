7062e91591cc46667611d9690baa440290796e35
= <=>
2025-08-09 12:42:30 -0300

    updated

---
+++ src/interfaces/order-with-products.interface.ts
@@ -0,0 +1,21 @@
+import { OrderStatus } from "generated/prisma";
+
+
+
+
+export interface OrderWithProducts {
+  OrderItem: {
+      name: any;
+      productId: number;
+      quantity: number;
+      price: number;
+  }[];
+  id: string;
+  totalAmount: number;
+  totalItems: number;
+  status: OrderStatus;
+  paid: boolean;
+  paidAt: Date | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
